e230caf65aa5a8158a88ad54e3413dd9edd49f71
khushi303 <maankhushi303@gmail.com>
2024-06-13 22:26:05 +0530

    chs

--- src/components/GaliledVideo.jsx
+++ src/components/GaliledVideo.jsx
@@ -1,33 +1,44 @@
-"use client"
-import Image from "next/image";
+"use client";
 import { useEffect, useState } from "react";
 
 export default function GaliledVideo() {
   const [currentVideo, setCurrentVideo] = useState(0);
+  const [isFading, setIsFading] = useState(false);
   const videos = [
     "/assets/video/gaming.mp4",
-    "/assets/video/gaming.mp4",
-    "/assets/video/gaming.mp4",
+    "/assets/video/controlGame.mp4",
+    "/assets/video/gameOver.mp4",
   ];
+
   useEffect(() => {
-    const videoElement = document.getElementById("background-video");
+    const videoElement = document.getElementById("bg-video");
     videoElement.src = videos[currentVideo];
     videoElement.play();
+    setIsFading(true); // Start fading in
     videoElement.addEventListener("ended", () => {
-      setCurrentVideo((currentVideo + 1) % videos.length);
-      videoElement.src = videos[currentVideo];
-      videoElement.play();
+      setCurrentVideo((prevVideo) => (prevVideo + 1) % videos.length);
+      setIsFading(false); // Reset fade-in effect for next video
     });
   }, [currentVideo]);
+
+  useEffect(() => {
+    if (isFading) {
+      const timer = setTimeout(() => {
+        setIsFading(false);
+      }, 1000); // Duration of fade-in effect
+      return () => clearTimeout(timer);
+    }
+  }, [isFading]);
+
   return (
     <div className="absolute inset-0 z-[1] pointer-events-none">
       <video
-        id="background-video"
+        id="bg-video"
         muted
         autoPlay
         playsInline
         controls={false}
-        className="w-full h-full object-cover"
+        className={`w-full h-full object-cover ${isFading ? "fade-in" : ""}`}
       />
     </div>
   );
